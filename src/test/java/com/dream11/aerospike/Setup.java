077eba6c0c91b985bc1142658130d3d68f33930e
Akshay Patidar <akshay.patidar@dream11.com>
2021-07-07 18:32:12 +0530

    test: add support for seed data

--- src/test/java/com/dream11/aerospike/Setup.java
+++ src/test/java/com/dream11/aerospike/Setup.java
@@ -1,10 +1,12 @@
 package com.dream11.aerospike;
 
+import java.io.IOException;
 import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.startupcheck.MinimumDurationRunningStartupCheckStrategy;
 
@@ -20,14 +22,18 @@ public class Setup implements BeforeAllCallback, AfterAllCallback, ExtensionCont
   }
 
   @Override
-  public void beforeAll(ExtensionContext extensionContext) {
+  public void beforeAll(ExtensionContext extensionContext) throws IOException, InterruptedException {
     aerospikeContainer = new GenericContainer<>(AEROSPIKE_IMAGE);
     log.info("Starting aerospike client from image:{}", AEROSPIKE_IMAGE);
     aerospikeContainer
+        .withEnv("NAMESPACE", "test")
         .withExposedPorts(3000)
         .withStartupTimeout(Duration.ofSeconds(1000))
-        .withStartupCheckStrategy(new MinimumDurationRunningStartupCheckStrategy(Duration.ofSeconds(10)));
+        .withStartupCheckStrategy(new MinimumDurationRunningStartupCheckStrategy(Duration.ofSeconds(10)))
+        .addFileSystemBind("src/test/resources/init.aql", "/aerospike-seed/init.aql", BindMode.READ_ONLY);
     aerospikeContainer.start();
+    // add seed data in containers
+    aerospikeContainer.execInContainer("aql", "-f", "aerospike-seed/init.aql");
     System.setProperty("aerospike.host", aerospikeContainer.getHost());
     System.setProperty("aerospike.port", String.valueOf(aerospikeContainer.getFirstMappedPort()));
   }
